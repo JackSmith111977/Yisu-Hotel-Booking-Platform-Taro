e90d89a9c1ec040fc4569f7803e3b05fe9af20c8
Kei <3526547131@qq.com>
2026-02-15 09:49:41 +0800

    chore: 配置基本路由结构

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,46 @@
 export default defineAppConfig({
-  pages: [
-    'pages/index/index'
+  // 主包页面
+  pages: ["pages/home/index", "pages/user/index"],
+  // 分包配置
+  subPackages: [
+    // 搜索分包
+    {
+      root: "packages/search",
+      pages: ["pages/list/index"],
+    },
+    // 酒店分包
+    {
+      root: "packages/hotel",
+      pages: [],
+    },
+    // 认证分包
+    {
+      root: "packages/auth",
+      pages: [],
+    },
   ],
+  // 3. TabBar 配置
+  tabBar: {
+    color: "#999",
+    selectedColor: "#fa2c19",
+    backgroundColor: "#fff",
+    list: [
+      {
+        pagePath: "pages/home/index",
+        text: "首页",
+        // iconPath: ...
+      },
+      {
+        pagePath: "pages/user/index",
+        text: "我的",
+        // iconPath: ...
+      },
+    ],
+  },
   window: {
-    backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
-  }
-})
+    backgroundTextStyle: "light",
+    navigationBarBackgroundColor: "#fff",
+    navigationBarTitleText: "WeChat",
+    navigationBarTextStyle: "black",
+  },
+});
---
+++ src/constants/route.ts
@@ -0,0 +1,12 @@
+export enum RoutePath {
+  // 主包
+  Home = "/pages/home/index",
+  User = "/pages/user/index",
+
+  // 搜索分包
+  SearchList = "/packages/search/pages/list/index",
+
+  // 酒店分包
+
+  // 认证分包
+}
